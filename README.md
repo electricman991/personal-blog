59a43f4f1f441a6da097b1707644de7e47e1f7ff
electricman991 <braidensommer@yahoo.com>
2024-07-12 01:44:06 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Personal Blog
+
+Based on the [tailwind-nextjs-starter-blog](https://github.com/timlrx/tailwind-nextjs-starter-blog) with all the features included in that template.
+
+## Licence
+
+[MIT](https://github.com/electricman991/personal-blog/blob/main/LICENSE) © [Braiden](https://www.github.com/electricman991)
---
+++ data/projectsData.ts
@@ -0,0 +1,24 @@
+interface Project {
+  title: string,
+  description: string,
+  href?: string,
+  imgSrc?: string,
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'Project Tracker',
+    description: `A project managing website that allows for small businesses to track and `,
+    imgSrc: '',
+    href: '/blog/project-tracker'
+  },
+  {
+    title: 'Package Tracker',
+    description: `A backend api that allows for the trucks to send information data and have it saved
+    and updated based on reltime data.`,
+    imgSrc: '',
+    href: '/blog/project-tracker'
+  }
+]
+
+export default projectsData
